a1c0bc720491957f903a89b95e06b1c34809e37a
vitalymuravyev <vitalymuravyev@gmail.com>
2020-08-04 17:26:16 +0300

    add view modules

---
+++ src/view/load-button.js
@@ -0,0 +1,3 @@
+export const createLoadButtonTemplate = () => {
+  return (` <button class="load-more" type="button">load more</button>`);
+};
---
+++ src/view/sort-list.js
@@ -0,0 +1,7 @@
+export const createSortListTemplate = () => {
+  return (
+    `<a href="#" class="board__filter" data-sort-type="default">SORT BY DEFAULT</a>
+    <a href="#" class="board__filter" data-sort-type="date-up">SORT BY DATE up</a>
+    <a href="#" class="board__filter" data-sort-type="date-down">SORT BY DATE down</a>`
+  );
+};
